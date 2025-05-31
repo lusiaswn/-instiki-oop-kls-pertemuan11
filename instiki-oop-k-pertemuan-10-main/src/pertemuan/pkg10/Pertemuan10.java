c4bd03c4486587c73f432477dcc7d230783e26c7
lusias <lusiasawun@gmail.com>
2025-05-31 12:10:10 +0800

    Keterangan commit

---
+++ instiki-oop-k-pertemuan-10-main/src/pertemuan/pkg10/Pertemuan10.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan.pkg10;
+
+/**
+ *
+ * @author INSTIKI
+ * Lusia Merry Mater Dolorosa Sawu
+ * TGL: 31 MEI 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
